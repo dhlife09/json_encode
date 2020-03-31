71148a24ccd69fa9e121ee347603baa8fe97e722
Dohyun Park <dhlife09@gmail.com>
2020-03-31 19:54:32 +0900

    Update parsing.php

--- parsing.php
+++ parsing.php
@@ -1,6 +1,6 @@
 <?php
 /* PHP JSON parser sample */
-    $data_str = file_get_contents('http://api.corona-19.kr/apitest');
+    $data_str = file_get_contents('http://service_address/index.php');
     $json = json_decode($data_str, true);
 	echo $json['result_code']. '<br>';
 	echo $json['result_message']. '<br>';
