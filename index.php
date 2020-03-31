565f9382f74331e6f33e8323e0c178e175528e73
DH <dhlife09@gmail.com>
2020-03-31 19:43:15 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+header('Content-Type: application/json; charset=UTF-8');	//JSON style
+// Declare an array  
+$a = '200';
+$b = 'ok';
+$value = array( 
+    "result_code"=>$a, 
+    "result_message"=>$b); 
+   
+// Use json_encode() function 
+$json = json_encode($value); 
+   
+// Display the output 
+echo($json); 
+   
+?> 
